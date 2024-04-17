14e55f68d52a2f9e2144df1c6a43e4847f8b326c
Akvilė <160050638+liudziuviene@users.noreply.github.com>
2024-04-17 22:38:32 +0300

    Add files via upload

---
+++ workFrom0417/src/main/java/org/example/workfrom0417/WorkFrom0417Application.java
@@ -0,0 +1,13 @@
+package org.example.workfrom0417;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkFrom0417Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkFrom0417Application.class, args);
+    }
+
+}
---
+++ workFrom0417/src/test/java/org/example/workfrom0417/WorkFrom0417ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.workfrom0417;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkFrom0417ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
